71e2a010c1ada3ce4d1f01c2a11cea579dd5ddf7
googlefan256 <googlefan@neody.dev>
2025-12-16 13:33:27 +0900

    feat: Add sound editing script

---
+++ sound.py
@@ -0,0 +1,21 @@
+# /// script
+# dependencies = [
+#   "pydub",
+#   "audioop-lts"
+# ]
+# ///
+from pydub import AudioSegment
+
+def edit_sound(f: str, reduction: int):
+    sound = AudioSegment.from_mp3(f"./bk/{f}")
+    quieter_sound = sound - reduction
+    quieter_sound.export(f"./src/assets/{f}", format="mp3")
+    print(f"完了しました: ./src/assets/{f} が作成されました")
+
+def main():
+    edit_sound("suho-medium.mp3", 9)
+    edit_sound("drive-medium.mp3", 6)
+    edit_sound("horse.mp3", 21)
+
+if __name__ == "__main__":
+    main()
